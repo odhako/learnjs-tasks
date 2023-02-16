211a9d4eed31ce8785eaa25dc422c8e828d88d2d
odhako <odhako@gmail.com>
2023-02-16 17:01:22 +0400

    2.10

---
+++ 2/10/2_script.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const answer = prompt('Какое «официальное» название JavaScript?', '');
+
+if (answer === 'ECMAScript') {
+  alert('Верно!');
+} else {
+  alert('Не знаете? ECMAScript!');
+}
---
+++ 2/10/4.js
@@ -0,0 +1,12 @@
+// let result;
+//
+// if (a + b < 4) {
+//   result = 'Мало';
+// } else {
+//   result = 'Много';
+// }
+
+
+let result;
+
+result = (a + b < 4) ? 'Мало' : 'Много';
